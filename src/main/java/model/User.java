e350fa03ebb4d80ddf606baf4779d99701941c2b
Zhastilek Zhalelov <zzhalelov@gmail.com>
2024-01-23 13:09:38 +0600

    Add @GeneratedValue

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -4,9 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Getter
 @Setter
@@ -15,6 +13,7 @@ import javax.persistence.Table;
 @ToString
 public class User {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private String email;
